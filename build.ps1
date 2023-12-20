82a0eea9918582a58d4fbbd86b4d244c44237da6
nicolas.salmieri <nicolas.salmieri@scalian.com>
2023-12-20 11:41:51 +0100

    refacto folder structure && clean dead scripts

--- build.ps1
+++
@@ -1,2 +0,0 @@
-# CMake build
-cmake -A x64 -B build -G "Visual Studio 17 2022" -D CMAKE_MODULE_PATH=build
---
+++ cpp/AuroraCLI/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+#include "AuroraAPI/Factorial.h"
+
+int main()
+{
+	// Configurer le logger
+	spdlog::set_pattern("[%H:%M:%S] [%t] %v");
+	spdlog::info("Logger initialized");
+
+	const int nbThreads = 1;
+	doSomethingParallel(nbThreads);
+
+	return 0;
+}
